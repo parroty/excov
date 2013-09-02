6c060cb36551c665baa0c9925bd8ea422fecc1c2
parroty <parroty00@gmail.com>
2013-09-02 21:31:20 +0900

    add test for Coveralls.start

--- test/coveralls_test.exs
+++ test/coveralls_test.exs
@@ -3,8 +3,15 @@ defmodule CoverallsTest do
   import Mock
 
   @stats_value [{{Coveralls, 1}, 0}, {{Coveralls, 2}, 1}]
-  test_with_mock "calculate stats", Cover, [analyze: fn(_module) -> {:ok, @stats_value} end] do
+  @modules [Coveralls]
+
+  test_with_mock "calculate stats", Cover, [analyze: fn(_) -> {:ok, @stats_value} end] do
     assert(Coveralls.calculate_stats([Coveralls]) == HashDict.new(@stats_value))
   end
 
+  test_with_mock "start", Cover, [compile: fn(_) -> nil end,
+                                  modules: fn -> @modules end,
+                                  analyze: fn(_) -> {:ok, @stats_value} end] do
+    assert(Coveralls.start("", [output: nil]) == :ok)
+  end
 end
